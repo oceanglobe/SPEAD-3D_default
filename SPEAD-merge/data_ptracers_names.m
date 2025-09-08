501336c981c3dcff1972ee5758c2b6b895cd22c4
GuillaumeLeGland <70918696+GuillaumeLeGland@users.noreply.github.com>
2025-09-08 09:17:25 +0200

    Add files via upload

---
+++ SPEAD-merge/data_ptracers_names.m
@@ -0,0 +1,22 @@
+function [PTRACERS_names] = data_ptracers_names()
+
+ PTRACERS_names{1}= 'NO3';
+ PTRACERS_names{2}= 'NO2';
+ PTRACERS_names{3}= 'NH4';
+ PTRACERS_names{4}= 'PO4';
+ PTRACERS_names{5}= 'FeT';
+ PTRACERS_names{6}= 'DOC';
+ PTRACERS_names{7}= 'POC';
+ PTRACERS_names{8}= 'c01';
+ PTRACERS_names{9}= 'xm01';
+ PTRACERS_names{10}= 'ym01';
+ PTRACERS_names{11}= 'zm01';
+ PTRACERS_names{12}= 'xxv01';
+ PTRACERS_names{13}= 'yyv01';
+ PTRACERS_names{14}= 'zzv01';
+ PTRACERS_names{15}= 'xyc01';
+ PTRACERS_names{16}= 'xzc01';
+ PTRACERS_names{17}= 'yzc01';
+ PTRACERS_names{18}= 'c02';
+
+ return
